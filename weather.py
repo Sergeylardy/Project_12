518de48781126499ca872fca452f002c8e73ee8d
Sergeylardy <100768646+Sergeylardy@users.noreply.github.com>
2022-03-02 14:21:23 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,26 @@
+# Для работы используется плагин pyowm
+#Задание №1 3а.
+
+from pyowm import OWM
+
+owm = OWM('b400fd18725654edcf9d064102da6409')
+place = input('Введи название вашего города')
+mgr = owm.weather_manager()
+observation = mgr.weather_at_place(place)
+w = observation.weather
+
+#Показатели
+#Температура
+t = w.temperature('celsius')
+t1 = t['temp']
+t2 = t['feels_like']
+t3 = t['temp_max']
+t4 = t['temp_min']
+
+#статус данных
+sts = w.status
+#Время
+time = w.reference_time('iso')
+#Вывод данных print
+
+print(f'В городе {place}, время {time} текущая температура{t1} градусов цельсия, ощущается как{t2} градусов цельсия, температура ночью{t4} градусов цельсия')
